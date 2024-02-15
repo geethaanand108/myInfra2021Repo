037638ab5002ab5eb32c85721574399181bf2dcd
geethaanand108 <152670645+geethaanand108@users.noreply.github.com>
2024-02-14 21:35:25 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-1" 
+       default     = "us-east-2" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    default     =  "gsjenkinskey" 
 }
 
 variable "instance_type" { 
     description = "instance type for ec2" 
-    default     =  "t2.micro" 
+    default     =  "t2.medium" 
 }
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "jenkins-sgroup-dec-2021" 
+    default     = "jenkins-sgroup-jan-2024" 
 }
 
 variable "tag_name" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ee23bfc74a881de5" 
+    default     = "ami-05fb0b8c1424f266b" 
 }
 variable "versioning" {
     type        = bool
